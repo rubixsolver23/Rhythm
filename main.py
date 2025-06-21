c3168f38991f67e4eefca54a43c376a395c354aa
rubixsolver23 <tylerclaywatts@gmail.com>
2025-06-21 14:16:38 -0600

    added basic pygame window

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import pygame
 import sys
 from pygame.locals import *
+import colors
 
 
-#wowowowowowowowoaaaaaaaaoaoaoaoh
-#fineI'll listen
+mainClock = pygame.time.Clock()
+pygame.init()
+
+WINDOWWIDTH = 600
+WINDOWHEIGHT = 600
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT), flags=pygame.SCALED, depth=32, vsync=1)
+pygame.display.set_caption("Rhythm")
+
+
+def main():
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+
+        windowSurface.fill(colors.WHITE)  # Clear the screen with black
+        pygame.display.update()  # Update the display
+        mainClock.tick(60)  # Limit to 60 frames per second
